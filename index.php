5a6ff3f2f721b275c5a89d065c75c9e9ddceb04e
yordy <yordyalejandro_14@hotmail.com>
2017-03-20 00:20:37 -0400

    Envios de correos Prueba 1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <title>Prueba Msj</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+        <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+	</head>
+<center><img src="images/portade.png" alt="..."></center>
+ 
+
+	<body >
+        <div class="wrap">
+            <header>
+                Prueba Envios de Correos desde Localhost
+            </header>
+            
+           <section id="principal">
+				<form id="formulario" method="post" action="php/enviar.php" enctype="multipart/form-data">
+					<div class="campos">			
+						<label>Para:</label>
+						<input type="email" name="email" required>
+					</div>
+					<div class="campos">
+						<label>Asunto:</label>
+						<input type="text" name="asunto">
+					</div>
+					<div class="campos">
+						<label>Mensaje:</label>
+						<textarea name="mensaje"></textarea>
+					</div>
+					
+					<label>Imagen:</label>
+					<input type="file" name="hugo" id="imagen" />
+								
+					<input id="submit" type="submit" name="enviar" value="Enviar mail">
+				</form>
+				
+			</section> 
+		</div>
+	</body>
+</html>
